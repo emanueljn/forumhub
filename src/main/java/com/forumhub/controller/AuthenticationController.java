16cba04be54b02a0cb9e31859a0300dc417f6cad
Emanuel J Nardes <emanuel.nardes@oabsp.org.br>
2024-07-02 16:39:05 -0300

    criar controller para o endpoint da autenticacao do usuario

---
+++ src/main/java/com/forumhub/controller/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.forumhub.controller;
+
+import com.forumhub.domain.user.DataAuthentication;
+import com.forumhub.domain.user.User;
+import com.forumhub.infra.security.TokenJWTData;
+import com.forumhub.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DataAuthentication dados) {
+        try {
+            var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+
+            var authentication = manager.authenticate(authenticationToken);
+
+            var tokenJWT =tokenService.gerarToken((User) authentication.getPrincipal());
+
+            return ResponseEntity.ok(new TokenJWTData(tokenJWT));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
